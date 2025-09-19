4c92c9bdbcfeda437cbbd76eaf5596ff0e0f8aa4
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-09-19 19:28:39 +0700

    chore: initialize monorepo with package.json and workspace setup for backend and frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
