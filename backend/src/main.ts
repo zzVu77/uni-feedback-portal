9632e9b4a354cd29cac8ce187f87f74a2fd4f2c6
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-09-22 21:00:32 +0700

    feat: enhance Prisma module with global scope and lifecycle management, setup SwaggerModule

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -8,9 +9,19 @@ async function bootstrap() {
     .setDescription('The Uni Portal Feedback API description')
     .setVersion('0.1')
     .build();
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1,
+    },
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
