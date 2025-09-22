4cb8d491446a802b4a7f72aed087aaab13f5545c
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-09-23 01:19:48 +0700

    fix: update return type of list method in CategoriesServiceContract

--- backend/src/categories/categories.service.contract.ts
+++ backend/src/categories/categories.service.contract.ts
@@ -2,5 +2,5 @@ import { QueryCategoriesDto } from './dto/query-categories.dto';
 import { CategoryResponseDto } from './dto/category-item.dto';
 
 export interface CategoriesServiceContract {
-  list(query: QueryCategoriesDto): Promise<{ items: CategoryResponseDto[] }>;
+  list(query: QueryCategoriesDto): Promise<CategoryResponseDto[]>;
 }
