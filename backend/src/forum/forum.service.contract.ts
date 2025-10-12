f96c9e880bfd9394ebe01babedfae2c44f83c0f2
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-12 09:41:28 +0700

    refactor: comment out unused createComment method in ForumServiceContract

--- backend/src/forum/forum.service.contract.ts
+++ backend/src/forum/forum.service.contract.ts
@@ -1,4 +1,4 @@
-import { CreateCommentDto } from './dto/create-comment.dto';
+// import { CreateCommentDto } from './dto/create-comment.dto';
 import { GetPostResponseDto } from './dto/get-post-param-response.dto';
 import { QueryCommentsDto } from './dto/query-comments.dto';
 import { QueryPostsResponseDto } from './dto/query-posts-response.dto';
@@ -17,16 +17,16 @@ export interface ForumServiceContract {
     post_id: number,
     user_id: number,
   ): Promise<{ voted: false; votes: number }>;
-  createComment(
-    dto: CreateCommentDto,
-    user_id: number,
-  ): Promise<{
-    comment_id: number;
-    post_id: number;
-    content: string;
-    created_at: string;
-    user: { user_id: number; full_name: string | null; role: string };
-  }>;
+  // createComment(
+  //   dto: CreateCommentDto,
+  //   user_id: number,
+  // ): Promise<{
+  //   comment_id: number;
+  //   post_id: number;
+  //   content: string;
+  //   created_at: string;
+  //   user: { user_id: number; full_name: string | null; role: string };
+  // }>;
   listComments(
     post_id: number,
     query: QueryCommentsDto,
