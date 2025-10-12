ec498425160db40e3d4de11d4efe086ea742ddb5
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-12 10:02:06 +0700

    refactor: comment out unused imports and class in update-forum.dto.ts; remove husky install script from package.json

--- backend/src/forum/dto/update-forum.dto.ts
+++ backend/src/forum/dto/update-forum.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateForumDto } from './create-forum.dto';
+// import { PartialType } from '@nestjs/mapped-types';
+// import { CreateForumDto } from './create-forum.dto';
 
-export class UpdateForumDto extends PartialType(CreateForumDto) {}
+// export class UpdateForumDto extends PartialType(CreateForumDto) {}
