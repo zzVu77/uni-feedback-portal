c8318d06ce70e08177be163f004f4cf2ae39871c
giahuynguyen28 <giahuy902@gmail.com>
2025-10-13 16:36:01 +0700

    Change snake-case to camel case

--- backend/src/comment/dto/create-comment.dto.ts
+++ backend/src/comment/dto/create-comment.dto.ts
@@ -1,5 +1,5 @@
 import { IsInt, IsString, Min, MinLength } from 'class-validator';
 export class CreateCommentDto {
-  @IsInt() @Min(1) post_id: number; // from path
+  @IsInt() @Min(1) postId: number; // from path
   @IsString() @MinLength(1) content: string;
 }
