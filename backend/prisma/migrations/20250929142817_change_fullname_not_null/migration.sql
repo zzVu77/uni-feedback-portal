f64ba3bf3e065b42f789b3b23b22cf96a3843fd6
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-14 16:51:44 +0700

    clean migration

--- backend/prisma/migrations/20250929142817_change_fullname_not_null/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `full_name` on table `users` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "public"."users" ALTER COLUMN "full_name" SET NOT NULL;
