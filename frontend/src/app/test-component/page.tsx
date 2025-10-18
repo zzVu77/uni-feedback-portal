7e2ca891a2497a979f81599e64490e8fc8d0b7ba
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-19 00:11:57 +0700

    feat: add StatusTimeline component with timeline items and status badges

--- frontend/src/app/test-component/page.tsx
+++ frontend/src/app/test-component/page.tsx
@@ -1,10 +1,11 @@
 import FeedbackDetailHeader from "@/components/feedback/FeedbackDetailHeader";
-import React from "react";
+import StatusTimeLine from "@/components/feedback/StatusTimeline";
 
 const page = () => {
   return (
     <div>
       <FeedbackDetailHeader />
+      <StatusTimeLine />
     </div>
   );
 };
