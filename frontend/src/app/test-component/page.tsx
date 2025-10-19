e6dccc2b941df2cd96753bc224dc6f6d46c50972
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-19 09:31:33 +0700

    feat: implement MessageItem component with user-specific styling and layout

--- frontend/src/app/test-component/page.tsx
+++ frontend/src/app/test-component/page.tsx
@@ -1,11 +1,28 @@
+import MessageItem from "@/components/conversation/MessageItem";
 import FeedbackDetailHeader from "@/components/feedback/FeedbackDetailHeader";
 import StatusTimeLine from "@/components/feedback/StatusTimeline";
 
 const page = () => {
   return (
-    <div>
+    <div className="py-10">
       <FeedbackDetailHeader />
       <StatusTimeLine />
+      <div className="flex flex-col gap-4 bg-white p-10">
+        <MessageItem
+          typeOfUser="STAFF"
+          isReceived={true}
+          content="Hello, how can I help you?"
+          name="John Doe"
+          timestamp="10:00 19/10/2025"
+        />
+        <MessageItem
+          typeOfUser="STUDENT"
+          isReceived={false}
+          content="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          name="John Doe"
+          timestamp="10:00 19/10/2025"
+        />
+      </div>
     </div>
   );
 };
