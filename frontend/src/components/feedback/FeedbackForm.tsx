6ee09225fc736b28b23ef8891e7f3d20ac28f136
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-21 00:34:10 +0700

    feat: implement FeedbackForm component with enhanced layout and scroll functionality

--- frontend/src/components/feedback/FeedbackForm.tsx
+++ frontend/src/components/feedback/FeedbackForm.tsx
@@ -26,6 +26,7 @@ import { Switch } from "../ui/switch";
 import { Textarea } from "../ui/textarea";
 import { FileInput } from "../common/FileInput";
 import { RotateCcw, Send } from "lucide-react";
+import { ScrollArea } from "../ui/scroll-area";
 const MAX_FILE_SIZE = 100 * 1024 * 1024; // 100MB
 const ACCEPTED_FILE_TYPES = [
   "image/jpeg",
@@ -81,7 +82,6 @@ const formSchema = z.object({
 const FeedbackForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    mode: "onTouched",
     defaultValues: {
       subject: "",
       feedbackCategory: "",
@@ -104,186 +104,198 @@ const FeedbackForm = () => {
     <>
       <Form {...form}>
         <form
-          className="flex flex-col gap-5 rounded-[8px] bg-white px-4 py-4 shadow-xs lg:px-8 lg:py-4"
+          className="flex flex-col gap-2 rounded-[8px] bg-white px-4 py-4 shadow-md lg:px-8 lg:py-4"
           onSubmit={form.handleSubmit(onSubmit)}
         >
           <span className="text-[20px] font-semibold lg:text-[28px]">
             Gửi góp ý đến nhà trường
           </span>
-          {/* Anonymous option */}
-          <FormField
-            control={form.control}
-            name="isPrivate"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <div className="bg-neutral-light-primary-200/40 flex flex-row items-center justify-between gap-4 rounded-[8px] px-5 py-2 shadow-xs">
-                    <div>
-                      <p className="text-[14px] font-medium lg:text-[16px]">
-                        Ẩn danh
-                      </p>
-                      <p className="text-muted-foreground text-[12px] font-normal lg:text-[14px]">
-                        Thông tin của bạn sẽ không hiển thị với cán bộ kiểm
-                        duyệt (nhưng ban quản trị cấp cao vẫn có thể xem được
-                        trong những trường hợp cần thiết).
-                      </p>
-                    </div>
-                    <Switch
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                      id="isPrivate"
-                      className="h-6 w-12 cursor-pointer bg-gray-300 shadow-sm data-[state=checked]:bg-blue-600 [&>span]:h-5 [&>span]:w-5 [&>span]:bg-white data-[state=checked]:[&>span]:translate-x-6"
-                    />
-                  </div>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* Subject Field */}
-          <FormField
-            control={form.control}
-            name="subject"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tiêu đề góp ý</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Mô tả ngắn gọn tiêu đề bạn muốn góp ý"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* Category Selection and Location Input */}
-          <div className="flex w-full flex-col items-start justify-between gap-5 lg:flex-row">
-            {/* Category Selection */}
-            <FormField
-              control={form.control}
-              name="feedbackCategory"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormLabel>Danh mục góp ý</FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Chọn danh mục" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Danh mục</SelectLabel>
-                          <SelectItem value="thuvien">Thư viện</SelectItem>
-                          <SelectItem value="giang_vien">Giảng viên</SelectItem>
-                          <SelectItem value="hoc_lieu">Học liệu</SelectItem>
-                          <SelectItem value="co_so_vat_chat">
-                            Cơ sở vật chất
-                          </SelectItem>
-                          <SelectItem value="khac">Khác</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  {/* <FormDescription>
-                      This is your public display name.
-                    </FormDescription> */}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            {/* Department Selection */}
-            <FormField
-              control={form.control}
-              name="department"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormLabel>Phòng ban tiếp nhận</FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Chọn phòng ban " />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Phòng ban</SelectLabel>
-                          <SelectItem value="khoa_dtqt">Khoa ĐTQT</SelectItem>
-                          <SelectItem value="khoa_cntt">Khoa CNTT</SelectItem>
-                          <SelectItem value="khoa_kinh_te">
-                            Khoa Kinh tế
-                          </SelectItem>
-                          <SelectItem value="khoa_ngoai_ngu">
-                            Khoa Ngoại ngữ
-                          </SelectItem>
-                          <SelectItem value="khac">Khác</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  {/* <FormDescription>
-                      This is your public display name.
-                    </FormDescription> */}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            {/* Location Input */}
-            <FormField
-              control={form.control}
-              name="location"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormLabel>Địa điểm cụ thể (nếu có)</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="w-full"
-                      placeholder="Ví dụ: Tầng 2, Phòng A101"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          {/* Description Field */}
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Mô tả chi tiết</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Mô tả chi tiết vấn đề bạn muốn góp ý"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* Attachments */}
-          <FormField
-            control={form.control}
-            name="attachments"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tài liệu đính kèm (nếu có)</FormLabel>
-                <FormControl>
-                  <FileInput value={field.value} onChange={field.onChange} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
+          <ScrollArea className="overflow-y-auto pr-4">
+            <div className="flex h-[76vh] flex-col gap-2">
+              {/* Anonymous option */}
+              <FormField
+                control={form.control}
+                name="isPrivate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <div className="bg-neutral-light-primary-200/40 flex flex-row items-center justify-between gap-4 rounded-[8px] px-5 py-2 shadow-xs">
+                        <div>
+                          <p className="text-[14px] font-medium lg:text-[16px]">
+                            Ẩn danh
+                          </p>
+                          <p className="text-muted-foreground text-[12px] font-normal lg:text-[14px]">
+                            Thông tin của bạn sẽ không hiển thị với cán bộ kiểm
+                            duyệt (nhưng ban quản trị cấp cao vẫn có thể xem
+                            được trong những trường hợp cần thiết).
+                          </p>
+                        </div>
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                          id="isPrivate"
+                          className="h-6 w-12 cursor-pointer bg-gray-300 shadow-sm data-[state=checked]:bg-blue-600 [&>span]:h-5 [&>span]:w-5 [&>span]:bg-white data-[state=checked]:[&>span]:translate-x-6"
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Subject Field */}
+              <FormField
+                control={form.control}
+                name="subject"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tiêu đề góp ý</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Mô tả ngắn gọn tiêu đề bạn muốn góp ý"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Category Selection and Location Input */}
+              <div className="flex w-full flex-col items-start justify-between gap-5 lg:flex-row">
+                {/* Category Selection */}
+                <FormField
+                  control={form.control}
+                  name="feedbackCategory"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Danh mục góp ý</FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Chọn danh mục" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectGroup>
+                              <SelectLabel>Danh mục</SelectLabel>
+                              <SelectItem value="thuvien">Thư viện</SelectItem>
+                              <SelectItem value="giang_vien">
+                                Giảng viên
+                              </SelectItem>
+                              <SelectItem value="hoc_lieu">Học liệu</SelectItem>
+                              <SelectItem value="co_so_vat_chat">
+                                Cơ sở vật chất
+                              </SelectItem>
+                              <SelectItem value="khac">Khác</SelectItem>
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      {/* <FormDescription>
+                          This is your public display name.
+                        </FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Department Selection */}
+                <FormField
+                  control={form.control}
+                  name="department"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Phòng ban tiếp nhận</FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Chọn phòng ban " />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectGroup>
+                              <SelectLabel>Phòng ban</SelectLabel>
+                              <SelectItem value="khoa_dtqt">
+                                Khoa ĐTQT
+                              </SelectItem>
+                              <SelectItem value="khoa_cntt">
+                                Khoa CNTT
+                              </SelectItem>
+                              <SelectItem value="khoa_kinh_te">
+                                Khoa Kinh tế
+                              </SelectItem>
+                              <SelectItem value="khoa_ngoai_ngu">
+                                Khoa Ngoại ngữ
+                              </SelectItem>
+                              <SelectItem value="khac">Khác</SelectItem>
+                            </SelectGroup>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      {/* <FormDescription>
+                          This is your public display name.
+                        </FormDescription> */}
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* Location Input */}
+                <FormField
+                  control={form.control}
+                  name="location"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Địa điểm cụ thể (nếu có)</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="w-full"
+                          placeholder="Ví dụ: Tầng 2, Phòng A101"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {/* Description Field */}
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Mô tả chi tiết</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Mô tả chi tiết vấn đề bạn muốn góp ý"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Attachments */}
+              <FormField
+                control={form.control}
+                name="attachments"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tệp đính kèm (nếu có)</FormLabel>
+                    <FormControl>
+                      <FileInput
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </ScrollArea>
           <div className="flex flex-row items-center justify-center gap-4 lg:justify-end">
             <Button
               onClick={handleResetForm}
