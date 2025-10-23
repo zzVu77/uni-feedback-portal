f31261740ef977314af0a9c093a5a7f2cd8cc3a1
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-23 11:28:44 +0700

    fix: remove unnecessary ESLint disable comments for type safety

--- frontend/src/components/feedback/my-feedbacks-history/MyFeedbacksHistoryTable.tsx
+++ frontend/src/components/feedback/my-feedbacks-history/MyFeedbacksHistoryTable.tsx
@@ -1,8 +1,4 @@
 "use client";
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   ColumnFiltersState,
   flexRender,
--- frontend/src/components/feedback/my-feedbacks-history/columns.tsx
+++ frontend/src/components/feedback/my-feedbacks-history/columns.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 import StatusBadge from "@/components/common/StatusBadge";
 import { Button } from "@/components/ui/button";
