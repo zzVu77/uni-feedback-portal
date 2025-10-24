e1b2797e011b54682f05780833290c45f797b93c
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-24 21:01:22 +0700

    feat: enhance OfficialResponse component with props for dynamic content

--- frontend/src/app/(layout)/forum/[id]/page.tsx
+++ frontend/src/app/(layout)/forum/[id]/page.tsx
@@ -7,7 +7,11 @@ const page = () => {
   return (
     <Wrapper>
       <FeedbackPostDetail />
-      <OfficialResponse />
+      <OfficialResponse
+        departmentName="Khoa đào tạo Quốc tế"
+        responseContent="Cảm ơn bạn đã góp ý. Chúng tôi hiện đang đánh giá tính khả thi của việc kéo dài thời gian hoạt động trong tuần thi cuối kỳ. Chúng tôi đang xem xét một chương trình thí điểm sẽ giữ cho tầng trệt mở cửa 24 giờ trong khi đóng cửa các tầng trên. Chúng tôi sẽ cập nhật cho mọi người về quyết định của mình vào cuối tháng."
+        responseDate="20/11/2023"
+      />
     </Wrapper>
   );
 };
--- frontend/src/components/forum/OfficialResponse.tsx
+++ frontend/src/components/forum/OfficialResponse.tsx
@@ -1,13 +1,21 @@
 import { BadgeCheck, Building2 } from "lucide-react";
 import React from "react";
-
-const OfficialResponse = () => {
+type Props = {
+  departmentName: string;
+  responseDate: string;
+  responseContent: string;
+};
+const OfficialResponse = ({
+  departmentName,
+  responseDate,
+  responseContent,
+}: Props) => {
   return (
     <div className="border-blue-primary-200 bg-blue-primary-100/40 flex w-full flex-col gap-3 rounded-[8px] border-1 px-4 py-2 shadow-sm">
       <div className="flex w-full flex-row items-center justify-start gap-1">
         <BadgeCheck className="text-blue-primary-700 h-5 w-5" />
         <span className="text-blue-primary-700 text-lg font-medium tracking-wide">
-          Trả lời từ nhà trường
+          Phản hồi từ nhà trường
         </span>
       </div>
       <div className="flex flex-row items-center justify-start gap-1">
@@ -15,20 +23,17 @@ const OfficialResponse = () => {
           <Building2 className="text-blue-primary-600" />
         </div>
         <div className="flex flex-col gap-1">
-          <span className="text-[18px] font-bold text-black/70">
-            Khoa Đào tạo quốc tế
+          <span className="text-[18px] font-semibold text-black/70">
+            {departmentName ?? "Khoa Đào tạo quốc tế"}
           </span>
-          <time className="text-neutral-dark-primary-100 text-sm font-normal">
-            20/10/2025
+          <time className="text-sm font-normal text-gray-400">
+            {responseDate ?? "20/10/2025"}
           </time>
         </div>
       </div>
       <p className="px-4 text-[14px]">
-        Cảm ơn bạn đã góp ý. Chúng tôi hiện đang đánh giá tính khả thi của việc
-        kéo dài thời gian hoạt động trong tuần thi cuối kỳ. Chúng tôi đang xem
-        xét một chương trình thí điểm sẽ giữ cho tầng trệt mở cửa 24 giờ trong
-        khi đóng cửa các tầng trên. Chúng tôi sẽ cập nhật cho mọi người về quyết
-        định của mình vào cuối tháng.
+        {responseContent ??
+          `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, fuga facilis? Expedita, perferendis corrupti harum quisquam quam dolores iusto voluptatum. Architecto eligendi optio molestiae odio ducimus accusantium id ipsa quisquam?`}
       </p>
     </div>
   );
