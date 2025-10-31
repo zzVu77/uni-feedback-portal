62e24f2cc0be2e0cc26b8eabddc93a3f730afdba
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-10-31 22:27:08 +0700

    feat: add NotificationItem component and integrate notifications in the test page

--- frontend/src/app/test-component/page.tsx
+++ frontend/src/app/test-component/page.tsx
@@ -5,10 +5,61 @@ import { MyFeedbacksHistoryTable } from "@/components/feedback/my-feedbacks-hist
 import StatusTimeLine from "@/components/feedback/StatusTimeline";
 
 import PostCard from "@/components/forum/PostCard";
+import NotificationItem from "@/components/notification/NotificationItem";
 
 const page = () => {
   return (
     <div className="space-y-3 px-5 py-10">
+      <NotificationItem
+        isRead={false}
+        type="ADMIN_NOTIFICATION"
+        time="2025-05-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="COMMENT_POST_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="FEEDBACK_PROCESSING_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="FEEDBACK_RECEIVED_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="FEEDBACK_REJECTED_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="FEEDBACK_RESOLVED_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="FEEDBACK_SUBMITTED_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="MESSAGE_NEW_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="MESSAGE_SYSTEM_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
+      <NotificationItem
+        isRead={false}
+        type="REPORT_COMMENT_NOTIFICATION"
+        time="2023-03-01T12:00:00Z"
+      />
       <PostCard />
       <PostCard />
       <PostCard />
