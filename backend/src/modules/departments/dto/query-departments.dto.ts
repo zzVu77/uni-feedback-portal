01892865ebf04d58598a473bfe49da46da7eae64
giahuynguyen28 <giahuy902@gmail.com>
2025-11-10 23:16:07 +0700

    feat: implement CRUD operations for Departments with validation and DTOs

--- backend/src/modules/departments/dto/query-departments.dto.ts
+++ backend/src/modules/departments/dto/query-departments.dto.ts
@@ -1,13 +1,47 @@
-import { IsInt, IsOptional, Min } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
+import {
+  IsBooleanString,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 
 export class QueryDepartmentsDto {
+  @ApiPropertyOptional({
+    description: 'Search term to filter departments by name or email.',
+    example: 'Computer',
+  })
+  @IsOptional()
+  @IsString()
+  q?: string;
+
+  @ApiPropertyOptional({
+    description:
+      "Filter departments by their active status ('true' or 'false'). If not provided, all departments are returned.",
+    example: 'true',
+  })
+  @IsOptional()
+  @IsBooleanString()
+  isActive?: string;
+
+  @ApiPropertyOptional({
+    description: 'The page number for pagination.',
+    default: 1,
+    minimum: 1,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1)
   page?: number;
 
+  @ApiPropertyOptional({
+    description: 'The number of items per page.',
+    default: 10,
+    minimum: 1,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsInt()
