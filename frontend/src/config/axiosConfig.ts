2ef37b9f7a62b6117f9ce40b82ebdeee3f7d255d
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-11-17 22:27:54 +0700

    feat: remove trailing slash from BASE_URL in axios configuration

--- frontend/src/config/axiosConfig.ts
+++ frontend/src/config/axiosConfig.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosError, AxiosInstance } from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080/";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080";
 
 interface AxiosInstanceCustom extends AxiosInstance {
   get<T = any>(url: string, config?: any): Promise<T>;
