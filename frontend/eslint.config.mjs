008407296fc994aab5702e2c51e65a1d099f31be
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-11-26 23:59:21 +0700

    feat: enhance ESLint configuration with additional TypeScript rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -38,6 +38,11 @@ export default tseslint.config(
         "error",
         { argsIgnorePattern: "^_" },
       ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-misused-promises": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
     },
   },
 );
