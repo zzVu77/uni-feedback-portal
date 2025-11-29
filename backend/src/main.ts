88e82dcb2b85454473b519abe77065bc475af4f6
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-11-29 21:21:01 +0700

    fix: update CORS configuration to use dynamic allowed origins and streamline port assignment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-
+  const allowedOrigins = [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL,
+    '*',
+  ];
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true,
   });
 
@@ -38,6 +42,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 8080);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
