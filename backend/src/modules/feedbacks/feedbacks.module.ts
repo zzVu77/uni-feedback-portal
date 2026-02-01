c327b06fa5416d6c4a932a8058f51c82935abbd5
Kien Mai <kientri098@gmail.com>
2026-02-01 16:49:05 +0700

    add aiservice into feedback module

--- backend/src/modules/feedbacks/feedbacks.module.ts
+++ backend/src/modules/feedbacks/feedbacks.module.ts
@@ -3,9 +3,10 @@ import { FeedbacksService } from './feedbacks.service';
 import { FeedbacksController } from './feedbacks.controller';
 import { ForumModule } from '../forum/forum.module';
 import { UploadsModule } from '../uploads/uploads.module';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
-  imports: [UploadsModule, ForumModule],
+  imports: [UploadsModule, ForumModule, AiModule],
   controllers: [FeedbacksController],
   providers: [FeedbacksService],
 })
