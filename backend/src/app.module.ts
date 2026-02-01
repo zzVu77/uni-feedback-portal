c708dc5b0b041b2bfa35d3fb5c791d364b3dd0a3
Kien Mai <kientri098@gmail.com>
2026-02-01 16:47:03 +0700

    create module ai

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,6 +21,7 @@ import { CommentModule } from './modules/comment/comment.module';
 import { MailModule } from './modules/mail/mail.module';
 import { RedisModule } from './modules/redis/redis.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AiModule } from './modules/ai/ai.module';
 
 @Module({
   imports: [
@@ -44,6 +45,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     CommentModule,
     MailModule,
     EventEmitterModule.forRoot(),
+    AiModule,
   ],
   controllers: [AppController],
   providers: [AppService],
