c92f0413ecb73b0e947234b9b17a15d717b04f89
Kien Mai <kientri098@gmail.com>
2026-02-01 16:47:31 +0700

    create module and service ai

---
+++ backend/src/modules/ai/ai.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { GoogleGenAI } from "@google/genai";
+import { toxicityPrompt,toxicKeywords } from './prompts/toxicity.prompt';
+@Injectable()
+export class AiService {
+  async checkToxicity(description: string): Promise<boolean> {
+    if(description){
+      // check keywords for toxicity
+      const toxicKeywordsList = toxicKeywords;
+      const contentLower = description.toLowerCase();
+      for (const keyword of toxicKeywordsList) {
+        if (contentLower.includes(keyword)) {
+          return true;
+        }
+      }
+      const API_KEY = process.env.API_GEMINI_KEY || '';
+      if(!API_KEY) return false;
+      const genAI = new GoogleGenAI({apiKey: API_KEY});
+      const prompt = toxicityPrompt(description);
+      const model = genAI.models.generateContent({
+          model: "gemini-2.5-flash",
+          contents: prompt
+      });
+      try{
+          const responseText = (await model).text;
+          const parsed = JSON.parse(responseText||'{"toxic": false}') as {toxic: boolean};
+          const isToxicAI = parsed.toxic;
+          if (isToxicAI) {
+            return true;
+          }
+      } catch (error) {
+          return false;
+      }
+    }
+    return false;
+  }
+}
