15eb7f91338930e6eabad9effc123ada1ada6cbf
Kien Mai <kientri098@gmail.com>
2026-02-05 01:04:45 +0700

    regist queue and add feedbacktoxicProcessor in feedbackmodule

--- backend/src/modules/feedbacks/feedbacks.module.ts
+++ backend/src/modules/feedbacks/feedbacks.module.ts
@@ -4,10 +4,16 @@ import { FeedbacksController } from './feedbacks.controller';
 import { ForumModule } from '../forum/forum.module';
 import { UploadsModule } from '../uploads/uploads.module';
 import { AiModule } from '../ai/ai.module';
+import { BullModule } from '@nestjs/bullmq';
+import { FeedbackToxicProcessor } from './feedbacks.processor';
 
 @Module({
-  imports: [UploadsModule, ForumModule, AiModule],
+  imports: [UploadsModule, ForumModule, AiModule,
+    BullModule.registerQueue({
+      name: 'feedback-toxic',
+    })
+  ],
   controllers: [FeedbacksController],
-  providers: [FeedbacksService],
+  providers: [FeedbacksService, FeedbackToxicProcessor],
 })
 export class FeedbacksModule {}
