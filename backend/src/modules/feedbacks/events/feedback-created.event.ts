dd91201276ece918a27010f9cbc8a0b6b4ec5287
KienLoveNH <kientri098@gmail.com>
2026-02-15 12:05:49 +0700

    refactor: add one field isToxic in feedbackcreatedevent

--- backend/src/modules/feedbacks/events/feedback-created.event.ts
+++ backend/src/modules/feedbacks/events/feedback-created.event.ts
@@ -1,10 +1,13 @@
 // src/modules/feedbacks/events/feedback-created.event.ts
 
+import is from "zod/v4/locales/is.js";
+
 export class FeedbackCreatedEvent {
   feedbackId: string;
   userId: string;
   departmentId: string;
   subject: string;
+  isToxic: boolean;
 
   constructor(partial: Partial<FeedbackCreatedEvent>) {
     Object.assign(this, partial);
