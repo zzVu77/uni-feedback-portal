98e4cde58d04f0ed61b5303b619e17587a83ba11
Kien Mai <kientri098@gmail.com>
2026-03-09 17:04:46 +0700

    refactor: update these fields change

--- backend/src/modules/feedbacks/dto/feedback-job-data.dto.ts
+++ backend/src/modules/feedbacks/dto/feedback-job-data.dto.ts
@@ -1,14 +1,12 @@
-import { CreateFeedbackDto } from "./create-feedback.dto";
 import type { ActiveUserData } from '../../auth/interfaces/active-user-data.interface';
 import { UpdateFeedbackDto } from "./update-feedback.dto";
 export type FeedbackJobData = | {
     type: 'create';
-    dto: CreateFeedbackDto;
+    feedback:any;
     actor: ActiveUserData;
 }|{
     type: 'update';
     feedbackId: string;
-    updateFileAttachments: any,
     updateData: any,
     dto: UpdateFeedbackDto;
     actor: ActiveUserData;
