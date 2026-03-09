912f3cd7d46e2f40d765eba47427d1a3df5d678c
Kien Mai <kientri098@gmail.com>
2026-03-09 17:05:47 +0700

    refactor: update processor

--- backend/src/modules/feedbacks/feedbacks.processor.ts
+++ backend/src/modules/feedbacks/feedbacks.processor.ts
@@ -1,62 +1,54 @@
-import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { OnWorkerEvent, Processor, WorkerHost  } from '@nestjs/bullmq';
 import { Job, UnrecoverableError } from 'bullmq';
 import { AiService } from '../ai/ai.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { ForumService } from '../forum/forum.service';
-import { UploadsService } from '../uploads/uploads.service';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { FeedbackCreatedEvent } from './events/feedback-created.event';
-import { GenerateStatusUpdateMessage } from 'src/shared/helpers/feedback-message.helper';
-import { FileTargetType } from '@prisma/client';
-import { CreateFeedbackDto, FeedbackSummary} from './dto';
+import { FeedbackStatus } from '@prisma/client';
 import type { ActiveUserData } from '../auth/interfaces/active-user-data.interface';
 import { FeedbackJobData } from './dto/feedback-job-data.dto';
 import { UpdateFeedbackDto } from './dto/update-feedback.dto';
-import { mergeStatusAndForwardLogs } from 'src/shared/helpers/merge-forwarding_log-and-feedback_status_history';
-import { FeedbackDetail } from './dto';
 @Processor('feedback-toxic')
 export class FeedbackToxicProcessor extends WorkerHost {
   constructor(
     private readonly aiService: AiService,
     private readonly prisma: PrismaService,
-    private readonly forumService: ForumService,
-    private readonly uploadsService: UploadsService,
     private readonly eventEmitter: EventEmitter2,
   ) {
     super();
   }
-  async process(
-    job: Job<FeedbackJobData,FeedbackSummary>): Promise<FeedbackSummary|FeedbackDetail> {
-        const {type} = job.data;
-        switch (type) {
-        case 'create':
-          return this.handleCreateFeedback(job.data);
+  async process(job: Job<FeedbackJobData>): Promise<void> {
+    const { type } = job.data;
+    switch (type) {
+      case 'create':
+        await this.handleCreateFeedback(job.data);
+        break;
+      case 'update':
+        await this.handleUpdateFeedback(job.data);
+        break;
 
-        case 'update':
-          return this.handleUpdateFeedback(job.data);
-
-        default:
-          throw new UnrecoverableError(`Unsupported job type: ${type}`);
-  }
-        
+      default:
+        throw new UnrecoverableError(`Unsupported job type: ${type}`);
+    }
   }
   async handleUpdateFeedback(data: {
-  feedbackId: string;
-  updateFileAttachments: any,
-  updateData: any,
-  dto: UpdateFeedbackDto;
-  actor: ActiveUserData;
-  }): Promise<FeedbackDetail> {
-    const {updateData,updateFileAttachments,feedbackId, dto, actor } = data;
-    const isToxic = await this.aiService.checkToxicity(dto.description || '');
-    if(isToxic){
-      const updateDataNew = {
-        ...updateData,
-        currentStatus: 'TOXIC',
-      };
+    feedbackId: string;
+    updateData: any;
+    dto: UpdateFeedbackDto;
+    actor: ActiveUserData;
+  }) {
+    const { updateData, feedbackId, dto, actor } = data;
+    const isToxic = await this.aiService.checkToxicity(
+      dto.description || '',
+      data,
+      'update',
+    );
+    if (isToxic) {
       await this.prisma.feedbacks.update({
         where: { id: feedbackId },
-        data: updateDataNew,
+        data: {
+          currentStatus: FeedbackStatus.VIOLATED_CONTENT,
+        },
         include: {
           department: {
             select: { id: true, name: true },
@@ -101,15 +93,12 @@ export class FeedbackToxicProcessor extends WorkerHost {
       throw new UnrecoverableError(
         'Feedback description contains toxic content. Please modify and try again.',
       );
-    }
-    else{
-      const updateDataNew = {
-        ...updateData,
-        currentStatus: "PENDING",
-      };
-      const updateFeedback = await this.prisma.feedbacks.update({
+    } else {
+      await this.prisma.feedbacks.update({
         where: { id: feedbackId },
-        data: updateDataNew,
+        data: {
+          currentStatus: FeedbackStatus.PENDING,
+        },
         include: {
           department: {
             select: { id: true, name: true },
@@ -142,119 +131,112 @@ export class FeedbackToxicProcessor extends WorkerHost {
           },
         },
       });
-
-      const unifiedTimeline = mergeStatusAndForwardLogs({
-        statusHistory: updateFeedback.statusHistory,
-        forwardingLogs: updateFeedback.forwardingLogs.map((f) => ({
-          fromDept: f.fromDepartment,
-          toDept: f.toDepartment,
-          message: f.message,
-          note: f.note ?? null,
-          createdAt: f.createdAt,
-        })),
+      // Emit Event: Toxic Feedback Updated
+      const feedbackCreatedEvent = new FeedbackCreatedEvent({
+        feedbackId: feedbackId,
+        userId: actor.sub,
+        departmentId: updateData.departmentId,
+        subject: updateData.subject,
+        isToxic: false,
       });
-      return {
-        id: updateFeedback.id,
-        isPublic: updateFeedback.forumPost ? true : false,
-        subject: updateFeedback.subject,
-        description: updateFeedback.description,
-        location: updateFeedback.location ? updateFeedback.location : null,
-        currentStatus: updateFeedback.currentStatus,
-        isPrivate: updateFeedback.isPrivate,
-        createdAt: updateFeedback.createdAt.toISOString(),
-        department: {
-          id: updateFeedback.department.id,
-          name: updateFeedback.department.name,
-        },
-        category: {
-          id: updateFeedback.category.id,
-          name: updateFeedback.category.name,
-        },
-        statusHistory: unifiedTimeline,
-        fileAttachments: updateFileAttachments,
-      };
+      this.eventEmitter.emit('feedback.created', feedbackCreatedEvent);
     }
-       
-
   }
-  async handleCreateFeedback(data: {
-    dto: CreateFeedbackDto;
-    actor: ActiveUserData;
-  }): Promise<FeedbackSummary> {
-     const { dto, actor } = data;
-        const { fileAttachments, ...feedbackData } = dto;
-        const isToxic = await this.aiService.checkToxicity(dto.description);
-        if (isToxic) {
-            const feedback = await this.prisma.feedbacks.create({
-            data: {
-                subject: feedbackData.subject,
-                description: feedbackData.description,
-                location: feedbackData.location,
-                isPrivate: dto.isAnonymous ? true : false,
-                departmentId: feedbackData.departmentId,
-                categoryId: feedbackData.categoryId,
-                currentStatus: 'TOXIC',
-                userId: actor.sub,
-            },
-            include: {
-                department: true,
-                category: true,
-            },
-            });
-            // Emit Event: Toxic Feedback Created
-            const feedbackCreatedEvent = new FeedbackCreatedEvent({
-              feedbackId: feedback.id,
-              userId: actor.sub,
-              departmentId: feedback.departmentId,
-              subject: feedback.subject,
-              isToxic: true,
-            });
-            this.eventEmitter.emit('feedback.created', feedbackCreatedEvent);
-            throw new UnrecoverableError(
-                'Feedback description contains toxic content. Please modify and try again.',
-            );
-        }
-        else{
-           // Create new feedback
-            const feedback = await this.prisma.feedbacks.create({
-            data: {
-                subject: feedbackData.subject,
-                description: feedbackData.description,
-                location: feedbackData.location,
-                isPrivate: dto.isAnonymous ? true : false,
-                departmentId: feedbackData.departmentId,
-                categoryId: feedbackData.categoryId,
-                userId: actor.sub,
-            },
-            include: {
-                department: true,
-                category: true,
-            },
-            });
-            if (dto.isPublic) {
-            await this.forumService.createForumPost(feedback.id, actor);
-            }
-
-      // Create attachments using UploadsService
-      if (fileAttachments && fileAttachments.length > 0) {
-        await this.uploadsService.updateAttachmentsForTarget(
-          feedback.id,
-          FileTargetType.FEEDBACK,
-          fileAttachments,
-        );
-      }
-
-      await this.prisma.feedbackStatusHistory.create({
+  async handleCreateFeedback(data: { feedback: any; actor: ActiveUserData }) {
+    const { feedback, actor } = data;
+    const isToxic = await this.aiService.checkToxicity(
+      feedback.description,
+      feedback,
+      'create',
+    );
+    if (isToxic) {
+      await this.prisma.feedbacks.update({
+        where: { id: feedback.id },
         data: {
-          feedbackId: feedback.id,
-          status: 'PENDING',
-          message: GenerateStatusUpdateMessage(
-            feedback.department.name,
-            'PENDING',
-          ),
+          currentStatus: FeedbackStatus.VIOLATED_CONTENT,
+        },
+        include: {
+          department: {
+            select: { id: true, name: true },
+          },
+          category: {
+            select: { id: true, name: true },
+          },
+          statusHistory: {
+            select: {
+              status: true,
+              message: true,
+              note: true,
+              createdAt: true,
+            },
+            orderBy: { createdAt: 'asc' },
+          },
+          forumPost: {
+            select: { id: true },
+          },
+          forwardingLogs: {
+            select: {
+              id: true,
+              message: true,
+              createdAt: true,
+              note: true,
+              fromDepartment: { select: { id: true, name: true } },
+              toDepartment: { select: { id: true, name: true } },
+            },
+            orderBy: { createdAt: 'asc' },
+          },
         },
       });
-
+      // Emit Event: Toxic Feedback Created
+      const feedbackCreatedEvent = new FeedbackCreatedEvent({
+        feedbackId: feedback.id,
+        userId: actor.sub,
+        departmentId: feedback.departmentId,
+        subject: feedback.subject,
+        isToxic: true,
+      });
+      this.eventEmitter.emit('feedback.created', feedbackCreatedEvent);
+      throw new UnrecoverableError(
+        'Feedback description contains toxic content. Please modify and try again.',
+      );
+    } else {
+      await this.prisma.feedbacks.update({
+        where: { id: feedback.id },
+        data: {
+          currentStatus: FeedbackStatus.PENDING,
+        },
+        include: {
+          department: {
+            select: { id: true, name: true },
+          },
+          category: {
+            select: { id: true, name: true },
+          },
+          statusHistory: {
+            select: {
+              status: true,
+              message: true,
+              note: true,
+              createdAt: true,
+            },
+            orderBy: { createdAt: 'asc' },
+          },
+          forumPost: {
+            select: { id: true },
+          },
+          forwardingLogs: {
+            select: {
+              id: true,
+              message: true,
+              createdAt: true,
+              note: true,
+              fromDepartment: { select: { id: true, name: true } },
+              toDepartment: { select: { id: true, name: true } },
+            },
+            orderBy: { createdAt: 'asc' },
+          },
+        },
+      });
       // [New Logic] Emit Event: Feedback Created
       // This allows the Notification module to handle notifications asynchronously without blocking this response.
       const feedbackCreatedEvent = new FeedbackCreatedEvent({
@@ -265,25 +247,34 @@ export class FeedbackToxicProcessor extends WorkerHost {
         isToxic: false,
       });
       this.eventEmitter.emit('feedback.created', feedbackCreatedEvent);
-
-      // Return summary
-      return {
-        id: feedback.id,
-        subject: feedback.subject,
-        location: feedback.location ? feedback.location : null,
-        currentStatus: feedback.currentStatus,
-        isPrivate: feedback.isPrivate,
-        department: {
-          id: feedback.department.id,
-          name: feedback.department.name,
-        },
-        category: {
-          id: feedback.category.id,
-          name: feedback.category.name,
-        },
-        createdAt: feedback.createdAt.toISOString(),
-      };
+    }
+  }
+  @OnWorkerEvent('failed')
+  async onFailed(job: Job, error: Error) {
+    if(job.attemptsMade >= (job.opts.attempts ?? 1)) {
+      if (job.data.type === 'create') {
+        const { feedback, actor } = job.data;
+        // Emit event for new feedback with FAULT_GEMINI status
+        const feedbackCreatedEvent = new FeedbackCreatedEvent({
+          feedbackId: feedback.id,
+          userId: actor.sub,
+          departmentId: feedback.departmentId,
+          subject: feedback.subject,
+          isToxic: true,
+        });
+        this.eventEmitter.emit('feedback.fault.api.gemini', feedbackCreatedEvent);
+      } else if (job.data.type === 'update') {
+        const { updateData, feedbackId, actor } = job.data;
+        // Emit event for new feedback with FAULT_GEMINI status
+        const feedbackCreatedEvent = new FeedbackCreatedEvent({
+          feedbackId: feedbackId,
+          userId: actor.sub,
+          departmentId: updateData.departmentId,
+          subject: updateData.subject,
+          isToxic: true,
+        });
+        this.eventEmitter.emit('feedback.fault.api.gemini', feedbackCreatedEvent);
       }
-    
+    }
   }
 }
