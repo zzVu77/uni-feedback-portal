7cc1c5e6c9609640760101663259cd7e556fbcee
Kien Mai <kientri098@gmail.com>
2026-04-30 00:36:13 +0700

    feat: ai service to departments proposal

--- backend/src/modules/ai/ai.service.ts
+++ backend/src/modules/ai/ai.service.ts
@@ -7,6 +7,12 @@ import { AiDataContext } from '../feedbacks/dto/feedback-job-data.dto';
 import { FeedbackDetail } from '../feedbacks/dto';
 import { UpdateFeedbackDto } from '../feedbacks/dto/update-feedback.dto';
 import { ActiveUserData } from '../auth/interfaces/active-user-data.interface';
+import { departmentProposalPrompt } from './prompts/department-proposal.prompt';
+import {
+  safeParseJSON,
+  DepartmentResponse,
+  DepartmentCandidate,
+} from './types/department-ai.dto';
 @Injectable()
 export class AiService {
   constructor(private readonly prisma: PrismaService) {}
@@ -137,5 +143,59 @@ export class AiService {
         },
       });
     }
+  }
+  async departmentProposal(description: string): Promise<DepartmentResponse> {
+    const API_KEY = process.env.API_GEMINI_KEY || '';
+    if (!API_KEY) {
+      throw new Error('Google Gemini API key is not configured.');
+    }
+    const genAI = new GoogleGenAI({ apiKey: API_KEY });
+
+    const candidates = await this.searchDepartments(description);
+    const prompt = departmentProposalPrompt(description, candidates);
+    const model = genAI.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents: prompt,
+    });
+    try {
+      const responseText = (await model).text;
+      const parsed = safeParseJSON(responseText || '{}');
+      return parsed;
+    } catch {
+      throw new Error('Error parsing AI response for department proposal.');
+    }
+  }
+  async embedDepartmentProposal(text: string) {
+    const API_KEY = process.env.API_GEMINI_KEY || '';
+    if (!API_KEY) {
+      throw new Error('Google Gemini API key is not configured.');
+    }
+    const genAI = new GoogleGenAI({ apiKey: API_KEY });
+    try {
+      const res = await genAI.models.embedContent({
+        model: 'gemini-embedding-2-preview',
+        contents: text,
+      });
+      return res.embeddings?.[0]?.values;
+    } catch {
+      throw new Error('Error generating department proposal embedding.');
+    }
+  }
+  async searchDepartments(description: string) {
+    const embedding = await this.embedDepartmentProposal(description);
+    // const vector = `[${embedding?.join(',')}]`;
+    const results = await this.prisma.$queryRawUnsafe(
+      `
+      SELECT d.id, d.name, d.description,
+         1 - (de.embedding <=> $1::vector) AS similarity
+      FROM "Departments" d
+      JOIN "DepartmentEmbeddings" de ON d.id = de."departmentId"
+      ORDER BY de.embedding <=> $1::vector
+      LIMIT 4
+    `,
+      embedding,
+    );
+
+    return results as DepartmentCandidate[];
   }
 }
